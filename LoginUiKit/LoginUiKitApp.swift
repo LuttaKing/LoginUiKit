5d875793f9bde37015b2e47997db3e5313ce556e
LuttaKing <denilsonwashuma@gmail.com>
2024-07-01 01:38:12 +0300

    Initial Commit

---
+++ LoginUiKit/LoginUiKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUiKitApp.swift
+//  LoginUiKit
+//
+//  Created by Denilson Washuma on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginUiKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
